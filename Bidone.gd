5762610f4480295d652297093ff1d3d4b9fb3009
gioele <gioele.santi@gmail.com>
2020-11-02 03:20:34 +0100

    Fix su tipi bidoni
    Il tipo gestio con export creava problemi, al contrario di active che funziona, quindi ho rimosso quella parte di codice ed ho inizializzato i colori dal Mondo. Ho inserito il codice per attivare i bidoni, ma bisogna inserire la disattivazione e risolvere il problema dell'animazione che non si mantiene statica alla fine.

--- Bidone.gd
+++ Bidone.gd
@@ -1,4 +1,3 @@
-tool
 extends KinematicBody2D
 class_name Bidone
 
@@ -6,7 +5,7 @@ onready var sprite = $Sprite
 
 # Impostazione tipo
 enum BIN_TYPE {BLUE, RED, GREEN, YELLOW}
-export (BIN_TYPE) var type := BIN_TYPE.GREEN setget set_type
+var type = BIN_TYPE.BLUE setget set_type
 var textures := {
 	'blue': "res://asset/sprite/bidone_blu.png",
 	'red': "res://asset/sprite/bidone_rosso.png",
@@ -19,17 +18,6 @@ var posizione=0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-func _unhandled_input(_event: InputEvent) -> void:
-	if not active: #muovo solo il bidone attivo
-		return
-	
-	#controllare le collisioni con altri bidoni
-	
-	#il movilmento avanto o inditro dovrebbe essere leggermente diagonale per rispettare la prospettiva (45°)
-	
-	get_tree().set_input_as_handled() #se il segnale è effettivamente processato, lo setto come hadled
-	pass
 
 func set_type(value) -> void:
 	type = value
--- Mondo.gd
+++ Mondo.gd
@@ -1,6 +1,11 @@
 extends Control
 
 onready var timer = get_node("Timer")
+onready var blu: Bidone = $Bidone/Blu
+onready var rosso: Bidone = $Bidone/Rosso
+onready var verde: Bidone = $Bidone/Verde
+onready var giallo: Bidone = $Bidone/Giallo
+var bidoni = [blu, rosso, verde, giallo]
 
 #in base al punteggio attuale, diminuisce il tempo di spawn
 #vedi spawnTime(score)
@@ -8,9 +13,26 @@ var score = 2
 
 func _ready() -> void:
 	timer.set_wait_time(spawnTime(score))
+	initialize()
+
+func initialize() -> void:
+	blu.type = 0
+	rosso.type = 1
+	verde.type = 2
+	giallo.type = 3 #vanno settati qui perché export rende la variabile globale
 
 func _process(delta: float) -> void:
 	timer.set_wait_time(spawnTime(score))
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("select_blue"):
+		blu.active = true
+	if event.is_action_pressed("select_red"):
+		rosso.active = true
+	if event.is_action_pressed("select_green"):
+		verde.active = true
+	if event.is_action_pressed("select_yellow"):
+		giallo.active = true
 
 func _on_Timer_timeout():
 	#una persona viene spawnata
