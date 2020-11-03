c14cd6f1d89ba21aeca97a101b13ff15dbdd1a2a
MauroMile <milella_mauro@live.it>
2020-11-03 11:43:46 +0100

    MAJOR CHANGES - MOVIMENTO
    Muri invisibili funzionanti sia ai lati che sopra e sotto; ora la scena Bidone ha 4 raycast e tutto il movimento è stato spostato in _input(event) -> movementInput( ).
    Il movimento funziona a gradoni.
    Lo spostamento sull'asse x e y si può vedere da codice (eventualmente possiamo metterlo export) e la lunghezza dei raycast è rispettivamente:
    raycast sinistro e destro: horizontalShift (vedere in script Bidone)
    raycast sopra e sotto: inclinato di boh tipo 20 gradi e la sua lunghezza è l'ipotenusa tra verticalShift_x e verticalShift_y

--- Bidone.gd
+++ Bidone.gd
@@ -14,8 +14,10 @@ var textures := {
 }
 
 var active := false setget set_active
-var posizione = 0
-# Called when the node enters the scene tree for the first time.
+var posizione = 0 #corsia 0 oppure 1
+var horizontalShift = 80 #numero pixel spostamento orrizontale
+var verticalShift_y = 30
+var verticalShift_x = 20
 
 var bumpable := true
 var bump_once := true
@@ -47,33 +49,34 @@ func set_active(value: bool) -> void:
 	else:
 		$AnimationPlayer.play("close")
 
-func _physics_process(delta):
-	#print($RayCast2D.get_collider())
-	if active==true:
-		var velocity=Vector2()
-		if Input.is_key_pressed(KEY_RIGHT):
-			velocity.x=+500
-		if Input.is_key_pressed(KEY_LEFT):
-				 velocity.x=-500
-		if Input.is_action_just_pressed("ui_up"):
-			if posizione==1:
-				if $RayCast2D.is_colliding() == false and $RayCast2D2.is_colliding() == false and velocity.x == 0: 
-					position.y-=40
-					position.x+=40
-					posizione=0
-		if Input.is_action_just_pressed("ui_down"):
-			if posizione==0:
-				if $RayCast2D.is_colliding() == false and $RayCast2D2.is_colliding() == false and velocity.x == 0 :
-					position.y+=40
-					position.x-=40
-					posizione=1
-		var collision = move_and_collide(velocity * delta)
-		if collision:
-			if collision.collider.has_method("_bump") and bump_once: 
-				bump_once = false
-				collision.collider._bump()
-		else:
-			bump_once = true
+func _input(event):
+	movementInput()
+
+func movementInput() -> void:
+	if(active == false):
+		return
+	var newPosition = get_position()
+	
+	if Input.is_action_just_pressed("ui_left"):
+		if($LeftRaycast.is_colliding() == false):
+			newPosition.x -= horizontalShift
+			
+	if Input.is_action_just_pressed("ui_right"):
+		if($RightRaycast.is_colliding() == false):
+			newPosition.x += horizontalShift
+			
+	if Input.is_action_just_pressed("ui_up"):
+		if($UpRaycast.is_colliding() == false):
+			newPosition.x += verticalShift_x
+			newPosition.y -= verticalShift_y
+	
+	if Input.is_action_just_pressed("ui_down"):
+		if($DownRaycast.is_colliding() == false):
+			newPosition.x -= verticalShift_x
+			newPosition.y += verticalShift_y
+	
+	($".").set_position(newPosition)
+		
 
 func _bump() -> void:
 	if bumpable:
@@ -85,14 +88,14 @@ func _on_Area2D_area_entered(area):
 	if(area is Rifiuto):
 		area.queue_free() #si potrebbe aggiungere un'animazione per farlo sparire gradualmente
 		if(area.type == self.type):
-			print("Punteggio e salute aumentati - suono _ok_")
+			scoreUp()
 			if(area.type == 3):
 				sfx = load("res://asset/audio/sfx/glass.wav")
 			else:
 				sfx = load("res://asset/audio/sfx/trash_in_can.wav")
 		else:
+			scoreDown()
 			$AnimationPlayer.play("full")
-			print("Salute diminuita - suono _hai sbagliato_")
 			sfx = load("res://asset/audio/sfx/wrong.wav")
 		$AudioStreamPlayer.stream = sfx
 		$AudioStreamPlayer.play()
@@ -101,3 +104,40 @@ func _on_Area2D_area_entered(area):
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "bump":
 		bumpable = true
+
+func scoreUp():
+	print("Punteggio e salute aumentati - suono _ok_")
+	pass
+
+func scoreDown():
+	print("Salute diminuita - suono _hai sbagliato_")
+	pass
+
+#deprecated _physics_process
+#func _physics_process(delta):
+#	#print($RayCast2D.get_collider())
+#	if active==true:
+#		var velocity=Vector2()
+#		if Input.is_key_pressed(KEY_RIGHT):
+#			velocity.x=+500
+#		if Input.is_key_pressed(KEY_LEFT):
+#				 velocity.x=-500
+#		if Input.is_action_just_pressed("ui_up"):
+#			if posizione==1:
+#				if $RayCast2D.is_colliding() == false and $RayCast2D2.is_colliding() == false and velocity.x == 0: 
+#					position.y-=40
+#					position.x+=40
+#					posizione=0
+#		if Input.is_action_just_pressed("ui_down"):
+#			if posizione==0:
+#				if $RayCast2D.is_colliding() == false and $RayCast2D2.is_colliding() == false and velocity.x == 0 :
+#					position.y+=40
+#					position.x-=40
+#					posizione=1
+#		var collision = move_and_collide(velocity * delta)
+#		if collision:
+#			if collision.collider.has_method("_bump") and bump_once: 
+#				bump_once = false
+#				collision.collider._bump()
+#		else:
+#			bump_once = true
